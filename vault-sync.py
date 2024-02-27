c0375dcf59a0b235b983dc58b3faedfc114c2a1b
Yerzhan Beisembayev <ybeisemb@redhat.com>
2024-02-27 16:24:07 -0500

    Added testing artifacts, started python code

--- vault-sync.py
+++ vault-sync.py
@@ -1,25 +1,51 @@
 from kubernetes import client, config, watch
-from pprint import pprint
+import os
+import logging
+import hvac
+
+def crd_watch():
+    apiClient = client.ApiClient()
+    customObjects = client.CustomObjectsApi(apiClient)
+    w = watch.Watch()
+    for event in w.stream(customObjects.list_cluster_custom_object, group="stable.example.com", version="v1alpha1", plural="vaultsyncs", 
+                          timeout_seconds=(os.environ.get("WATCH_TIMEOUT") if os.environ.get("WATCH_TIMEOUT") else 3600)):
+        logging.info("Checking vaultsync resource %s in namespace %s" % (event['object']['metadata']['name'], event['object']['metadata']['namespace']))
 
 
 def main():
-    # Configs can be set in Configuration class directly or using helper
-    # utility. If no argument provided, the config will be loaded from
-    # default location.
+    # Set logging format
+    logging.basicConfig(format='%(asctime)s %(message)s', level=(logging.DEBUG if os.environ.get("DEBUG") else logging.INFO))
+
+    # Assuming this script runs as K8S pod or kubeconfig is authenticated to cluster
     config.load_kube_config()
 
-    apiClient = client.ApiClient()
-    customObjects = client.CustomObjectsApi(apiClient)
-#    count = 10
-    for i in (1,2):
-        w = watch.Watch()
-        for event in w.stream(customObjects.list_cluster_custom_object, group="stable.example.com", version="v1alpha1", plural="vaultsyncs", timeout_seconds=1000):
-            print("Event: %s %s: %s" % (event['type'], event['object']['metadata']['namespace'], event['object']['metadata']['name']))
-#            pprint(event['object']['metadata']['name'])
-#        count -= 1
-#        if not count:
-#            w.stop()
-    print("Finished configmaps stream.")
+    # Authenticate to Vault using env vars 
+    vault_addr = os.environ.get("VAULT_ADDR")
+    vault_token = os.environ.get("VAULT_TOKEN")
+    vault_role_id = os.environ.get("VAULT_ROLE_ID")
+    vault_secret_id = os.environ.get("VAULT_SECRET_ID")
+    if not vault_token:
+        logging.info("Authenticating to Vault %s using AppRole." %(vault_addr))
+        vault_client = hvac.Client(
+            url=vault_addr
+        )
+        vault_client.auth.approle.login(
+            role_id=vault_role_id,
+            secret_id=vault_secret_id
+        )
+    else:
+        logging.info("Authenticating to Vault %s using Token." %(vault_addr))
+        vault_client = hvac.Client(
+            url=vault_addr,
+            token=vault_token
+        )
+
+    # main loop
+    while (True):
+        logging.info("(Re)Starting watch loop")
+        crd_watch()
+
+    logging.info("Exiting.")
 
 
 if __name__ == '__main__':
